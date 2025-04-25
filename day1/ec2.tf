1f34e7b5eade5168e40d656557f7d3de31e08dfc
nayra000 <youssefnayra26@gmail.com>
2025-04-25 19:18:04 +0300

    Upload Lab Day 1

---
+++ day1/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "my-key"
+  public_key = file("../keys/my-key.pub") 
+}
+
+
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-0e449927258d45bc4" 
+  instance_type = "t2.micro"             
+  subnet_id     = aws_subnet.public_subnet1.id
+  security_groups = [
+    aws_security_group.allow_ssh_anywhere.id
+  ]
+  key_name = aws_key_pair.ssh_key. key_name
+
+  tags = {
+    Name = "Bastion-Host"
+  }
+}
+
+resource "aws_instance" "application" {
+  ami           = "ami-0e449927258d45bc4" 
+  instance_type = "t2.micro"             
+  subnet_id     = aws_subnet.private_subnet1.id
+  security_groups = [
+    aws_security_group.allow_ssh_and_3000_vpc.id
+  ]
+  key_name = aws_key_pair.ssh_key. key_name
+
+  tags = {
+    Name = "Application-Host"
+  }
+}
+
---
+++ day1/subnet.tf
@@ -0,0 +1,40 @@
+resource "aws_subnet" "public_subnet1" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  
+  map_public_ip_on_launch = true 
+  tags = {
+    Name = "public_subnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a" 
+
+  tags = {
+    Name = "private_subnet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b" 
+  map_public_ip_on_launch = true  
+
+  tags = {
+    Name = "public_subnet2"
+  }
+}
+
+resource "aws_subnet" "private_subnet2" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b" 
+
+  tags = {
+    Name = "private_subnet2"
+  }
+}
