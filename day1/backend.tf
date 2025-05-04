57263065b9f70148684a6a429913d21091f9b4b3
nayra000 <youssefnayra26@gmail.com>
2025-05-05 00:05:10 +0300

    add Jenkinsfile

--- day1/backend.tf
+++ day1/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "state-bucket-586794467501"       
-    key            = "state-file" 
-    region         = "us-east-1"                 
-    encrypt        = true                         
-    dynamodb_table = "terraform-locks"             
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "state-bucket-586794467501"       
+#     key            = "state-file" 
+#     region         = "us-east-1"                 
+#     encrypt        = true                         
+#     dynamodb_table = "terraform-locks"             
+#   }
+# }
